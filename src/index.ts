ec7f876e7a0484bc0f15ad8fe5bf4ed24ff68d82
yonghun16 <yonghun16@naver.com>
2025-01-22 01:55:46 +0900

    Declartion Files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-class Block {
-  constructor(
-    private data: string
-  ) {}
-  static hello() {
-    return 'hello';
-  }
-}
+import {init, exit} from 'myPackage';
+
+init({
+  url: "true"
+});
+
+exit(1)
